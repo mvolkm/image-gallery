1484774961517eea7e07eeb0763f84f386a5c8d6
Michael Volkmar <michael.volkmar@gmail.com>
2021-05-24 11:25:07 +0200

    display images on Card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 import ImageCard from './components/ImageCard';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
@@ -18,7 +19,7 @@ const App = () => {
     )
       .then((res) => res.json())
       .then((data) => {
-        SetImages([data, ...images]);
+        SetImages([{ ...data, title: word }, ...images]);
       })
       .catch((err) => {
         console.log(err);
@@ -28,9 +29,19 @@ const App = () => {
 
   return (
     <div>
-      <Header title="Images Gallery" />
+      <Header title="Bilder Galerie" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
-      <ImageCard />
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3}>
+          {images.map((image, i) => {
+            return (
+              <Col key={i} className="pb-3">
+                <ImageCard key={i} image={image} />
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
     </div>
   );
 };
--- frontend/src/components/ImageCard.js
+++ frontend/src/components/ImageCard.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 
-const ImageCard = () => {
+const ImageCard = ({ image }) => {
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Img variant="top" src={image.urls.small} />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Card.Title>{image.title}</Card.Title>
+        <Card.Text>{image.description || image.alt_description}</Card.Text>
+        <Button variant="dark">Delete</Button>
       </Card.Body>
     </Card>
   );
