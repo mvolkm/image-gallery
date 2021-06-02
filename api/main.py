5f5939f12e902abb5f8721cedc3e8753aadeaf76
Michael Volkmar <michael.volkmar@gmail.com>
2021-06-02 08:41:25 +0200

    enabled pylint and blck Code formater for python

--- api/main.py
+++ api/main.py
@@ -4,39 +4,41 @@ from flask import Flask, request
 from dotenv import load_dotenv
 from flask_cors import CORS
 
-#import der lokalen Variablen, die nicht ins Git sollen
+
 load_dotenv(dotenv_path="./.env.local")
 
-#Setzen von Variablen
-UNSPLASH_URL="https://api.unsplash.com/photos/random"
-UNSPLASH_KEY=os.environ.get("UNSPLASH_KEY", "")
-DEBUG=bool(os.environ.get("DEBUG", True))
+# Setzen von Variablen
+UNSPLASH_URL = "https://api.unsplash.com/photos/random"
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
+DEBUG = bool(os.environ.get("DEBUG", True))
 
-#Wenn kein KEY gefunden wurde, wird das Programm abgebrochen
+# Wenn kein KEY gefunden wurde, wird das Programm abgebrochen
 if not UNSPLASH_KEY:
-    raise EnvironmentError("Please create .env.local file and insert there UNSPLASH_KEY")
-#Definition des AppNamens
+    raise EnvironmentError(
+        "Please create .env.local file and insert there UNSPLASH_KEY"
+    )
+# Definition des AppNamens
 app = Flask(__name__)
 CORS(app)
-#Status des DEBUG Modus
+# Status des DEBUG Modus
 app.config["DEBUG"] = DEBUG
-#Anlegen des Pfades für den Flask Server
+# Anlegen des Pfades für den Flask Server
+
+
 @app.route("/new-image")
-#Funktion des
+# Funktion des
 def new_image():
     word = request.args.get("query")
 
-    headers={
-        "Accept-Version": "v1",
-        "Authorization": "Client-ID " + UNSPLASH_KEY
-    }
-    params = {
-        "query": word
-    }
+    headers = {
+        "Accept-Version": "v1", "Authorization": "Client-ID " + UNSPLASH_KEY
+        }
+    params = {"query": word}
 
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
     data = response.json()
     return data
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
